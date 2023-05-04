2a62aea8340ec3f3327765fdb73127ec2afffab1
leo <leonardofarena@gmail.com>
2023-05-04 09:52:28 -0300

    bb

--- src/main/java/com/lfa/ecomerceapp/config/ServletInitializer.java
+++ src/main/java/com/lfa/ecomerceapp/config/ServletInitializer.java
@@ -9,5 +9,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(EcomerceSpringJavaApplication.class);
+        //bbbbbbb
     }
 }
